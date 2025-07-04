129d4c163b20429b4002d95698d2217f2f3acc9b
NikitaSemenukha <skarabaychick@gmail.com>
2025-07-04 19:23:40 +0200

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import { notification } from 'antd';
 
 const API_URL = 
 process.env.REACT_APP_API_URL || process.env.VITE_API_URL || 
-'http://localhost:3000/api';
+// 'http://localhost:3000/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
